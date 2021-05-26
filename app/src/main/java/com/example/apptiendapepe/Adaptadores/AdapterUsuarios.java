02413c0fb0bfd3f145dd0a87bc1f03f26bcae720
Manuel1330 <manueloficial2001@gmail.com>
2021-05-26 23:51:23 +0200

    Initial commit

---
+++ app/src/main/java/com/example/apptiendapepe/Adaptadores/AdapterUsuarios.java
@@ -0,0 +1,101 @@
+package com.example.apptiendapepe.Adaptadores;
+
+import android.content.Context;
+        import android.content.Intent;
+        import android.net.Uri;
+        import android.view.LayoutInflater;
+        import android.view.View;
+        import android.view.ViewGroup;
+        import android.widget.ImageView;
+        import android.widget.LinearLayout;
+        import android.widget.TextView;
+        import android.widget.Toast;
+
+        import androidx.annotation.NonNull;
+        import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.apptiendapepe.Datos.Usuarios;
+import com.example.apptiendapepe.R;
+
+import java.util.ArrayList;
+
+public class AdapterUsuarios extends RecyclerView.Adapter<AdapterUsuarios.ViewHolderUsuarios> implements View.OnClickListener{
+
+    ArrayList<Usuarios> listaUsuarios;
+    private Context contexto;
+    private View.OnClickListener listener;
+
+    public AdapterUsuarios(ArrayList<Usuarios> listaUsuarios, Context contexto) {
+        this.listaUsuarios = listaUsuarios;
+        this.contexto = contexto;
+    }
+
+    @NonNull
+    @Override
+    public AdapterUsuarios.ViewHolderUsuarios onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_list_usuarios, null, false);
+
+        view.setOnClickListener(this);
+
+        return new ViewHolderUsuarios(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull AdapterUsuarios.ViewHolderUsuarios holder, int position) {
+        Usuarios usu = listaUsuarios.get(position);
+
+        holder.txtNombre.setText(usu.getPersonName());
+        holder.txtId.setText(usu.getPersonId());
+        Glide.with(contexto).load(usu.getPersonPhoto()).centerCrop().into(holder.foto);
+
+        holder.foto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        holder.item.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                return false;
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return listaUsuarios.size();
+    }
+
+    public void setOnClickListener(View.OnClickListener listener){
+        this.listener = listener;
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (listener!=null){
+            listener.onClick(v);
+        }
+    }
+
+    public class ViewHolderUsuarios extends RecyclerView.ViewHolder{
+
+        TextView txtNombre, txtId;
+        ImageView foto;
+        LinearLayout item;
+
+
+        public ViewHolderUsuarios(@NonNull View itemView) {
+            super(itemView);
+
+            txtNombre= itemView.findViewById(R.id.idNombre);
+            txtId= itemView.findViewById(R.id.idId);
+            foto= itemView.findViewById(R.id.idImagen);
+            item = itemView.findViewById(R.id.idItem);
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/apptiendapepe/Fragments/UsuariosFragment.java
@@ -0,0 +1,68 @@
+package com.example.apptiendapepe.Fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.apptiendapepe.Adaptadores.AdapterUsuarios;
+import com.example.apptiendapepe.Datos.Usuarios;
+import com.example.apptiendapepe.R;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+
+public class UsuariosFragment extends Fragment{
+
+    ArrayList<Usuarios> listaUsuarios;
+
+    RecyclerView recyclerUsuarios;
+
+    public UsuariosFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_usuarios, container, false);
+        recyclerUsuarios = view.findViewById(R.id.recyclerId);
+
+        listaUsuarios = new ArrayList<>();
+        recyclerUsuarios.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        llenarUsuarios();
+
+        AdapterUsuarios adapter = new AdapterUsuarios(listaUsuarios,getContext());
+
+        recyclerUsuarios.setAdapter(adapter);
+
+        return view;
+    }
+
+    private void llenarUsuarios() {
+
+        FirebaseDatabase.getInstance().getReference("usuario").get().addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                task.getResult().getChildren().forEach(child -> {
+                    Usuarios usuario = child.getValue(Usuarios.class);
+                    listaUsuarios.clear();
+                    listaUsuarios.add(usuario);
+                });
+            }
+        });
+
+    }
+
+}
